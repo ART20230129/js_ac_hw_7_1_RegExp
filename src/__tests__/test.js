3bb20093a344acafa7b4808c73272e6185658cb1
ART <tow1979@gmail.com>
2024-04-02 22:18:37 +0300

    sec commit

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,31 +1,26 @@
 import Validator from '../js/nickname';
 
-test('Checking for a pattern match', () =>{
-        const unit = new Validator('tes-_123t');
-        expect(unit.validateUsername()).toBeTruthy();
+test('Checking for a pattern match', () => {
+  const unit = new Validator('tes-_123t');
+  expect(unit.validateUsername()).toBeTruthy();
+});
 
-})
+test('Checking for the number at the beginning of the line', () => {
+  const unit = new Validator('2test');
+  expect(unit.validateUsername()).toBeFalsy();
+});
 
-test('Checking for the number at the beginning of the line', () =>{
-        const unit = new Validator('2test');
-        expect(unit.validateUsername()).toBeFalsy();
+test('Checking for the number at the end of the line', () => {
+  const unit = new Validator('test3');
+  expect(unit.validateUsername()).toBe(false);
+});
 
-})
+test('Checking for the number of digits in the name', () => {
+  const unit = new Validator('t-1234est');
+  expect(unit.validateUsername()).toBe(false);
+});
 
-test('Checking for the number at the end of the line', () =>{
-        const unit = new Validator('test3');
-        expect(unit.validateUsername()).toBe(false);
-
-})
-
-test('Checking for the number of digits in the name', () =>{
-        const unit = new Validator('t-1234est');
-        expect(unit.validateUsername()).toBe(false);
-
-})
-
-test('Checking for extraneous characters', () =>{
-        const unit = new Validator('tes!t');
-        expect(unit.validateUsername()).toBe(false);
-
-})
+test('Checking for extraneous characters', () => {
+  const unit = new Validator('tes!t');
+  expect(unit.validateUsername()).toBe(false);
+});
--- src/js/nickname.js
+++ src/js/nickname.js
@@ -1,10 +1,10 @@
-
 export default class Validator {
-        constructor(nick){
-                this.nick = nick;
-        }
-        validateUsername(){
-                const re = /^[a-z][a-z0-9\-_]+[a-z]$/i;
-                return (re.exec(this.nick) !== null)&&(!/[0-9]{4}/.test(this.nick));    
-        }
+  constructor(nick) {
+    this.nick = nick;
+  }
+
+  validateUsername() {
+    const re = /^[a-z][a-z0-9\-_]+[a-z]$/i;
+    return (re.exec(this.nick) !== null) && (!/[0-9]{4}/.test(this.nick));
+  }
 }
